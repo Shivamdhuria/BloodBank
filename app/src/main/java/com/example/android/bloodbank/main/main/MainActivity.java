b6efe88d1ffa6961a1b85b81d3717039d912adfb
Shivam <shivam.dhuria07@gmail.com>
2018-08-29 01:17:17 +0530

    Tabbed Activity done

--- app/src/main/java/com/example/android/bloodbank/main/main/MainActivity.java
+++ app/src/main/java/com/example/android/bloodbank/main/main/MainActivity.java
@@ -1,87 +1,84 @@
 package com.example.android.bloodbank.main.main;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 import com.example.android.bloodbank.R;
-import com.example.android.bloodbank.main.intro.IntroActivity;
-import com.example.android.bloodbank.main.newcampaign.NewCampaignActivity;
 import com.example.android.bloodbank.main.signin.SignInActivity;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
 
-    private FirebaseAuth mAuth;
-    FloatingActionButton floatingActionButton;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        floatingActionButton = findViewById(R.id.fab);
+        toolbar = (Toolbar) findViewById(R.id.htab_toolbar);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+        viewPager = (ViewPager) findViewById(R.id.htab_viewpager);
+        setupViewPager(viewPager);
 
+        tabLayout = (TabLayout) findViewById(R.id.htab_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
-        checkIfFirstTime();
-
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Intent intentNewCampaign = new Intent(getApplicationContext(), NewCampaignActivity.class);
-                startActivity(intentNewCampaign);
-
-            }
-        });
 
     }
 
-    private void checkIfFirstTime() {
-        final String PREFS_NAME = "MyPrefsFile";
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Requests");
+        adapter.addFragment(new TwoFragment(),"Responses");
 
+        viewPager.setAdapter(adapter);
+    }
 
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-        if (settings.getBoolean("my_first_time", true)) {
-            //the app is being launched for first time, do something
-            Log.d("Comments", "First time");
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
 
-            // first time task
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
 
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
 
-            // record the fact that the app has been started at least once
-            settings.edit().putBoolean("my_first_time", false).commit();
+         void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
 
-            Intent intentIntro = new Intent(this, IntroActivity.class);
-            startActivity(intentIntro);
-        } else {
-            // Check if user is signed in (non-null) and update UI accordingly.
-
-            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-            updateUI(currentUser);
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
     }
-
-
-
-
-
 
 
 
@@ -90,12 +87,10 @@ public class MainActivity extends AppCompatActivity {
 
         if(user != null){
             //TODO GET DETAILS FROM DB
-            Log.e("Main Activity","User not null"+ user.getUid());
 
         }else{
 
             //Send Back to Login
-            Log.e("Main Activity","User  null");
             Intent intentLogin = new Intent(this, SignInActivity.class);
             startActivity(intentLogin);
         }
---
+++ app/src/main/java/com/example/android/bloodbank/main/main/TwoFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.bloodbank.main.main;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.bloodbank.R;
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_two_fragment, container, false);
+    }
+
+}
